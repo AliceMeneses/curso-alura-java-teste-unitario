68a26c71eb955b8e76084151b543bffce0c59d44
Alice <am171696@alunos.unisanta.br>
2020-12-03 09:34:49 -0300

    Retirando bug do metodo avalia

--- src/br/com/caelum/leilao/servico/Avaliador.java
+++ src/br/com/caelum/leilao/servico/Avaliador.java
@@ -4,29 +4,27 @@ import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 
 public class Avaliador {
-	
+
 	double maiorValor = Double.NEGATIVE_INFINITY;
 	double menorValor = Double.POSITIVE_INFINITY;
-	
-	public void avalia(Leilao leilao) {		
-		
-		for(Lance lance : leilao.getLances()) {
-			
-			if(lance.getValor() > maiorValor) {
+
+	public void avalia(Leilao leilao) {
+
+		for (Lance lance : leilao.getLances()) {
+
+			if (lance.getValor() > maiorValor) {
 				maiorValor = lance.getValor();
 			}
-			else {
-				if(lance.getValor() < menorValor) {
-					menorValor = lance.getValor();
-				}
+			if (lance.getValor() < menorValor) {
+				menorValor = lance.getValor();
 			}
 		}
 	}
-	
+
 	public double getMaiorLance() {
 		return maiorValor;
 	}
-	
+
 	public double getMenorValor() {
 		return menorValor;
 	}
