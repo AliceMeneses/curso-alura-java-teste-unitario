11106a26266203788a284125f152fa2201bf6f48
Alice <am171696@alunos.unisanta.br>
2020-12-05 07:55:55 -0300

    Adicionando teste para o Leilao receber um lance

--- test/br/com/caelum/leilao/dominio/TestaLeilao.java
+++ test/br/com/caelum/leilao/dominio/TestaLeilao.java
@@ -1,10 +1,25 @@
 package br.com.caelum.leilao.dominio;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestaLeilao {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@Test
+	public void deveReceberUmLance() {
+		
+		Usuario alice = new Usuario("Alice");
+		
+		Leilao leilao = new Leilao("Livro");
+		
+		assertEquals(0, leilao.getLances().size());
+		
+		leilao.propoe(new Lance(alice, 30));
+		
+		assertEquals(1, leilao.getLances().size());
+		assertEquals(30, leilao.getLances().get(0).getValor(), 0.0001);
 	}
 
 }
