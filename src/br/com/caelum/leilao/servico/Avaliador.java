604c374e84c98d9f3534aa0667abb8b55f1ce45c
Alice <am171696@alunos.unisanta.br>
2020-12-06 16:42:01 -0300

    Lançando exceção se o leilao não possuir lances

--- src/br/com/caelum/leilao/servico/Avaliador.java
+++ src/br/com/caelum/leilao/servico/Avaliador.java
@@ -15,6 +15,10 @@ public class Avaliador {
 	List<Lance> maiores;
 
 	public void avalia(Leilao leilao) {
+		
+		if(leilao.getLances().size() == 0) {
+			throw new RuntimeException("Não é possível avaliar leilão sem lances!");
+		}
 
 		for (Lance lance : leilao.getLances()) {
 
